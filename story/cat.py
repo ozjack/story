fb74d2a8fb2e804e05f502ba5f495c5c7e93d4ce
Oscar <Oscar Ames>
2015-09-12 18:55:45 -0400

    save

---
+++ story/cat.py
@@ -0,0 +1,7 @@
+"""The Cat
+
+You approach the cat slowely from the North and it hisses as you.
+You can go back or maybe something else.
+"""
+
+on_back="intro"
---
+++ story/intro.py
@@ -0,0 +1,6 @@
+"""Welcome to my story!
+
+You see a cat dimmly in the distance.
+"""
+
+on_cat="cat"
